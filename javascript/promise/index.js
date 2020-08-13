49977edb9835d074d2e7e5a9495a6ad412b1b3e1
anggimaulinda <anggimaulinda21@gmail.com>
2020-08-13 20:58:53 +0800

    promise

---
+++ javascript/promise/index.js
@@ -0,0 +1,36 @@
+console.log("hallo");
+
+let data = []
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then(response => response.json())
+.then(data => {
+    console.log('data todos', data)
+    document.getElementById('todos').innerHTML = `${data.map(todo => `<div>${todo.title}</div></br>`)}`
+});
+
+
+
+const promiseA = (data) =>
+  new Promise((resolve, reject) => {
+    if (data === 1) {
+      resolve('success');
+    }
+    reject('failed');
+});
+
+// Pending
+// Resolved
+// Reject
+
+// console.log('promise A', promiseA())
+
+promiseA(1)
+  .then((val) => console.log("asynchronous logging has val:", val)) // return succes value
+  .catch(err => console.log('error', err)) // return error
+
+
+console.log("immediate logging");
+
+const test = () => console.log('test')
+test()
