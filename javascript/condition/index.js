1800b17f6d19e9f92e27e29207bca7880da8915b
anggimaulinda <anggimaulinda21@gmail.com>
2020-09-04 20:25:38 +0800

    edit console

--- javascript/condition/index.js
+++ javascript/condition/index.js
@@ -1,91 +1,93 @@
 const products = [
-    {
-        type: 'shoes',
-        color: 'red',
-        size: [38, 39, 40],
-        brand: 'Adidas',
-    },
-    {
-        type: 'shoes',
-        color: 'black',
-        size: [36, 38],
-        brand: 'Adidas'
-    },
-    {
-        type: 'shoes',
-        color: 'gray',
-        size: [40, 41],
-        brand: 'Nike'
-    },
-    {
-        type: 'bag',
-        color: 'black',
-        size: null,
-        brand: 'Prada'
-    },
-    {
-        type: 'bag',
-        color: 'yellow',
-        size: [30],
-        brand: 'Hermes'
-    },
-    {
-        type: 'bag',
-        color: 'brown',
-        size: [30, 35, 40],
-        brand: 'Hermes'
-    },
-    {
-        type: 'jacket',
-        color: 'white',
-        size: ['S', 'M'],
-        brand: 'Chanel'
-    }
-]
+  {
+    type: "shoes",
+    color: "red",
+    size: [38, 39, 40],
+    brand: "Adidas",
+  },
+  {
+    type: "shoes",
+    color: "black",
+    size: [36, 38],
+    brand: "Adidas",
+  },
+  {
+    type: "shoes",
+    color: "gray",
+    size: [40, 41],
+    brand: "Nike",
+  },
+  {
+    type: "bag",
+    color: "black",
+    size: null,
+    brand: "Prada",
+  },
+  {
+    type: "bag",
+    color: "yellow",
+    size: [30],
+    brand: "Hermes",
+  },
+  {
+    type: "bag",
+    color: "brown",
+    size: [30, 35, 40],
+    brand: "Hermes",
+  },
+  {
+    type: "jacket",
+    color: "white",
+    size: ["S", "M"],
+    brand: "Chanel",
+  },
+];
 
-const bags = products.filter(product => {
-    // Return only shoes
-    // if() {
-    //     return product
-    // }
-})
+const bags = products.filter((product) => {
+  // Return only shoes
+  // if() {
+  //     return product
+  // }
+});
 
-console.log(bags);
+console.log('bags', bags);
 
+const bagJacket = products.filter((product) => {
+  // Return bag and jacket
+  // if() {
+  //     return product
+  // }
+});
 
-const shoesJacket = products.filter(product => {
-    // Return bag and jacket
-    // if() {
-    //     return product
-    // }
-})
+console.log('bag and jacket', bagJacket);
 
-console.log(shoesJacket);
+const redShoes = products.filter((product) => {
+  // Return shoes with color red
+  // if() {
+  //     return product
+  // }
+});
 
-const redShoes = products.filter(product => {
-    // Return shoes with color red
-    // if() {
-    //     return product
-    // }
-})
+console.log('red shoes', redShoes);
 
-console.log(redShoes)
+const shoesSize40 = products.filter((product) => {
+  // Return shoes with size 40
+  // if() {
+  //     return product
+  // }
+});
 
-const shoesSize40 = products.filter(product => {
-    // Return shoes with size 40
-    // if() {
-    //     return product
-    // }
-})
+console.log('shoes size 40', shoesSize40);
 
-console.log(shoesSize40)
+// Return bag with brand Hermes size 35.
+// If any also show chanel jacket
+// If not show all hermes
+const hermesBag = products.filter((product) => {
 
-// Return bag with brand Hermes size 35. 
-    // If any also show chanel jacket
-    // If not show all hermes
-const productRecomendation = //write code here
+});
 
-console.log(productRecomendation)
+const productRecomendation = // write your code here
 
+console.log('Hermes bag', hermesBag);
 
-
+console.log('product recomendation', productRecomendation);
